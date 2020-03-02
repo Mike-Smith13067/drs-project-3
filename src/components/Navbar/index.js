3277c9a64857c4364f5e13bb978ef6ab0a886049
Mike <hockeydad66@live.com>
2020-03-02 12:21:09 -0700

    component base code

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="/">
+        Navigation Bar
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
