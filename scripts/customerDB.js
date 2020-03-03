771fd96c66e2bf8f711b230affeb2de91048f16b
Mike-Smith13067 <55055850+Mike-Smith13067@users.noreply.github.com>
2020-03-02 20:55:50 -0700

    added drs-automotive db and a few more components (#1)

---
+++ scripts/customerDB.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the customer collection and inserts the customer below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/drs-automotive"
+);
+
+const customer = [
+  {
+    name: "Mike Smith",
+    make: "Jeep",
+    model:"2006 Wrangler X",
+    vin: "xxxxx",
+    service: new Date(Date.now()),
+    notes: "asf"
+  },
+];
+
+db.drs-automotive
+  .remove({})
+  .then(() => db.drs-automotive.collection.insert(customer))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
