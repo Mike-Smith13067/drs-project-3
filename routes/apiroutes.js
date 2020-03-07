c8156df180889ed3d5aadd7424fb67c4da7b1d93
Mike <hockeydad66@live.com>
2020-03-07 13:58:07 -0700

    updated routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,12 +2,31 @@ var db = require("../models")
 function apiroutes(app) {
 
      app.get("/api/customers", function(req, res) {
-         console.log("test")
          db.Customer.find({}).then(function(data) {
             res.json(data)
-         })
+         }).catch(function(err) {
+             res.json(err);
+         });
         
-     })
+     });
+
+     app.get("/api/customers/:id", function(req, res) {
+        db.Customer.findOne({_id: req.params.id}).then(function(data) {
+           res.json(data)
+        }).catch(function(err){
+            res.json(err);
+        });
+       
+    });
+
+    app.delete("/api/customers/delete/:id", function(req, res) {
+        db.Customer.findOneAndRemove({_id:req.params.id}).then(function(data) {
+           res.json(data)
+        }).catch(function(err){
+            res.json(err);
+        });
+       
+    });
 }
 
 module.exports = apiroutes
