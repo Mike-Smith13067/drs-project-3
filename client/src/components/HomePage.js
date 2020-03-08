da6ac8fec81fbba9faa29fcd0ca7f0e137157ee0
Mike <hockeydad66@live.com>
2020-03-08 16:35:07 -0600

    Home page is displaying something

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import NavTabs from "./Navbar";
+import Home from "./pages/Home";
+import Customer from "./pages/Customer";
+import Inventory from "./pages/Inventory";
+import Contact from "./pages/Contact";
+
+class HomePage extends Component {
+  state = {
+    currentPage: "Home"
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  renderPage = () => {
+    if (this.state.currentPage === "Home") {
+      return <Home />;
+    } else if (this.state.currentPage === "Customer") {
+      return <Customer />;
+    } else if (this.state.currentPage === "Inventory") {
+      return <Inventory />;
+    } else {
+      return <Contact />;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <NavTabs
+          currentPage={this.state.currentPage}
+          handlePageChange={this.handlePageChange}
+        />
+        {this.renderPage()}
+      </div>
+    );
+  }
+}
+
+export default HomePage;
