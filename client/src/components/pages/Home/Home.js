45a6256b5b44f9c148db83dd14ccf1d7dbcf4780
Steven Stot <sstott09@yahoo.com>
2020-03-14 15:46:58 -0600

    Updated Carousel

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -6,10 +6,20 @@ const image2 = require("../Home/images/image2.jpg")
 const image3 = require("../Home/images/image3.jpg")
 const image4 = require("../Home/images/image4.jpg")
 const image5 = require("../Home/images/image5.jpg")
-const image6 = require("../Home/images/image6.jpg")
-const image7 = require("../Home/images/image7.jpg")
 
+const h20 = "";
+const h21 = "Diagnostic Test";
+const h22 = "Brake Test";
+const h23 = "Fluid Change";
+const h24 = "Tune Up";
+const h25 = "Tire Services";
 
+const p0 = "";
+const p1 = "Whether your vehicle is showing obvious signs of a problem or you noticed the check engine light is on, a quick car diagnostic test by an expert technician can help pinpoint the problem.";
+const p2 = "The brake inspection typically involves checking the brake components according to manufacturer's guidelines. For example, measuring the brake pad or brake shoe material thickness and inspecting brake discs. If there are signs that pads or caliper guide (slide) pins are not moving freely, DRS Automotive may recommend servicing your brakes.The disc brake service includes removing the pads and calipers, cleaning and lubricating guide pins and the caliper brackets to prevent sticking or seizing. The brake discs (rotors) might also need to be cleaned out from the surface rust, especially on the edges, to prevent squeaking.";
+const p3 = "Checking fluid levels regularly and changing the fluids and filters periodically can minimize the risks of breakdowns and prolong the life of the engine, transmission, cooling system and brakes. So, if you are driving a (maintenance challenged) vehicle, isn't it about time to start paying closer attention to the fluids and filters?";
+const p4 = "Regular engine tune-ups restore power and efficiency back to your car. At DRS Auto Services we'll visually inspect your engine components. Install new parts (including spark plugs, fuel and air filters) as necessary, adjust timing and idle settings if applicable, and provide fuel system cleaning services to restore fuel system performance.  By following your vehicle manufacturer's recommendations, you're investing in your vehicle's longevity. Saving you time and money in the long run.  Contact us today to find out additional information.";
+const p5 = "Properly maintained tires will give you a safer, more comfortable ride and a longer tread life. Tire component properties evolve over time. For each tire, this evolution depends upon many factors such as weather, storage conditions, and conditions of use (load, speed, inflation pressure, maintenance etc.) to which the tire is subjected throughout its life. In addition to regular inspections and inflation pressure maintenance by consumers, it is recommended to have passenger car tires and light truck tires, including spare tires, inspected regularly by a qualified tire specialist, such as a tire dealer, who will assess the tire's suitability for continued service.";
 class Home extends Component {
   componentDidMount() {
     const slides = document.querySelectorAll('.slide');
@@ -18,26 +28,38 @@ class Home extends Component {
     const auto = true; // Auto scroll
     const intervalTime = 4000;
     let counter = 1;
+    let counterh2 = 1;
+    let counterp = 1;
     let slideInterval;
-    
-
     const nextSlide = () => {
       counter++
-      if(counter >= 8) {
-        counter =0
-
+      if (counter >= 6) {
+        counter = 0
       }
-
-      var imageArray = [ image0, image1, image2, image3, image4, image5, image6, image7]; 
+      counterh2++
+      if (counterh2 >= 6) {
+        counterh2 = 0
+      }
+      counterp++
+      if (counterp >= 6) {
+        counterp = 0
+      }
+      var imageArray = [image0, image1, image2, image3, image4, image5];
+      var h2Array = [h20, h21, h22, h23, h24, h25];
+      var pArray = [p0, p1, p2, p3, p4, p5];
       var image = imageArray[counter];
+      var subheader = h2Array[counterh2];
+      var subtext = pArray[counter];
       var currentImage = document.getElementById("current-image");
       currentImage.setAttribute("src", image);
-
-
-
-      return image;
+      var currentSubheader = document.getElementById("subheader");
+      currentSubheader.innerHTML = "";
+      currentSubheader.append(subheader);
+      var currentSubtext = document.getElementById("subtext");
+      currentSubtext.innerHTML = "";
+      currentSubtext.append(subtext);
+      return image, subheader, subtext;
     };
-
     const prevSlide = () => {
       // Get current class
       const current = document.querySelector('.current');
@@ -53,7 +75,6 @@ class Home extends Component {
       }
       setTimeout(() => current.classList.remove('current'));
     };
-
     // Button events
     next.addEventListener('click', e => {
       nextSlide();
@@ -62,7 +83,6 @@ class Home extends Component {
         slideInterval = setInterval(nextSlide, intervalTime);
       }
     });
-
     prev.addEventListener('click', e => {
       prevSlide();
       if (auto) {
@@ -70,67 +90,23 @@ class Home extends Component {
         slideInterval = setInterval(nextSlide, intervalTime);
       }
     });
-
     // Auto slide
     if (auto) {
       // Run next slide at interval time
       slideInterval = setInterval(nextSlide, intervalTime);
     }
   }
-
-
   render() {
-
     return (
       <div>
         <div className="slider">
           <div className="slide current">
-          <a href="https://flyclipart.com/automotive-mechanic-car-repair-retro-auto-repair-clip-art-517846" title="Automotive Mechanic Car Repair Retro - Auto Repair Clip Art"><img src={require("../Home/images/image0.jpg")} width="95%" alt="drs-logo" id = "current-image"/></a>
+            <a href="https://flyclipart.com/automotive-mechanic-car-repair-retro-auto-repair-clip-art-517846" title="Automotive Mechanic Car Repair Retro - Auto Repair Clip Art"><img src={require("../Home/images/image0.jpg")} width="95%" alt="drs-logo" id="current-image" /></a>
           </div>
           <div className="slide">
             <div className="content">
-              <h2></h2>
-              <p></p>
-            </div>
-          </div>
-          <div className="slide">
-            <div className="content">
-              <h2></h2>
-              <p></p>
-            </div>
-          </div>
-          <div className="slide">
-            <div className="content">
-              <h2>Diagnostic Tests</h2>
-              <p>Whether your vehicle is showing obvious signs of a problem or you noticed the âcheck engineâ light is on, a quick car diagnostic test by an expert technician can help pinpoint the problem.</p>
-            </div>
-          </div>
-          <div className="slide">
-            <div className="content">
-              <h2>Brake Service</h2>
-              <p>The brake inspection typically involves checking the brake components according to manufacturer's guidelines. For example, measuring the brake pad or brake shoe material thickness and inspecting brake discs. If there are signs that pads or caliper guide (slide) pins are not moving freely, DRS Automotive may recommend servicing your brakes.
-              <br></br>
-              The disc brake service includes removing the pads and calipers, cleaning and lubricating guide pins and the caliper brackets to prevent sticking or seizing. The brake discs (rotors) might also need to be cleaned out from the surface rust, especially on the edges, to prevent squeaking. </p>
-            </div>
-          </div>
-          <div className="slide">
-            <div className="content">
-              <h2>Oil &amp; Fluid Change</h2>
-              <p>Checking fluid levels regularly and changing the fluids and filters periodically can minimize the risks of breakdowns and prolong the life of the engine, transmission, cooling system and brakes. So, if you are driving a "maintenance challenged" vehicle, isn't it about time to start paying closer attention to the fluids and filters? </p>
-            </div>
-          </div>
-          <div className="slide">
-            <div className="content">
-              <h2>Tune Ups</h2>
-              <p>Regular engine tune-ups restore power and efficiency back to your car. At DRS Auto Services we'll visually inspect your engine components. Install new parts (including spark plugs, fuel and air filters) as necessary, adjust timing and idle settings if applicable, and provide fuel system cleaning services to restore fuel system performance.  By following your vehicle manufacturer's recommendations, you're investing in your vehicle's longevity. Saving you time and money in the long run.  Contact us today to find out additional information. </p>
-            </div>
-          </div>
-          <div className="slide">
-            <div className="content">
-              <h2>Tire Services</h2>
-              <p>Properly maintained tires will give you a safer, more comfortable ride and a longer tread life. Tire component properties evolve over time. For each tire, this evolution depends upon many factors such as weather, storage conditions, and conditions of use (load, speed, inflation pressure, maintenance etc.) to which the tire is subjected throughout its life.
-                <br></br>
-              In addition to regular inspections and inflation pressure maintenance by consumers, it is recommended to have passenger car tires and light truck tires, including spare tires, inspected regularly by a qualified tire specialist, such as a tire dealer, who will assess the tire's suitability for continued service.  </p>
+              <h2 id="subheader"></h2>
+              <p id="subtext"></p>
             </div>
           </div>
         </div>
@@ -138,7 +114,6 @@ class Home extends Component {
           <button id="prev"><i className="fas fa-arrow-left"></i></button>
           <button id="next"><i className="fas fa-arrow-right"></i></button>
         </div>
-
       </div>)
   }
 }
