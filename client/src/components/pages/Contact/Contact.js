6d59509993dad363c079171a45e0d1887b823825
Alex H <hampton.alex10@gmail.com>
2020-03-21 13:38:14 -0600

    contact/footer

--- client/src/components/pages/Contact/Contact.js
+++ client/src/components/pages/Contact/Contact.js
@@ -1,24 +1,39 @@
 import React from "react";
 import Calendar from "react-calendar";
 
-class Calendarpage extends React.Component {
-  state = {
-    currentMonth: new Date(),
-    selectedDate: new Date()
-  }
-
-  onChange = date => this.setState({ date })
- 
+class Card extends React.Component {
   render() {
-    return (
-      <div>
-        <Calendar
-          onChange={this.onChange}
-          value={this.state.date}
-        />
-      </div>
-    );
+    return(
+        <div className="card">
+          <img src={this.props.img} />
+          <div className="card-body">
+            <h2>{this.props.title}</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            <h5>{this.props.author}</h5>
+          </div>
+        </div>
+    )
   }
 }
 
-export default Calendarpage;
+class App extends React.Component {
+  render() {
+    return (
+      <>
+       <div class="header">
+         <h1>React Card Component</h1>
+         <h3>A simple and responsive card component in React with clean hover effects. Copy and customise.</h3>
+       </div>
+      
+       <div className='cards'>
+         <Card
+          img='https://picsum.photos/id/54/400/300'
+          title='What I learned from my visit to The Upside Down'
+          author='Nancy Wheeler' />
+      </div>  
+      </>
+    )
+  }
+}
+
+export default Card; 
