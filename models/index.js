8fbada096cba21bb8dabb727bcadd62ac83809b3
Mike <hockeydad66@live.com>
2020-03-27 13:09:24 -0600

    fixing routes

--- models/index.js
+++ models/index.js
@@ -2,5 +2,5 @@ module.exports = {
     Customer: require("./customer.js"),
     Inventory: require("./inventory.js"),
     Admin: require("./admin.js"),
-    WorkOrder: require("./workOrder.js")
+    WorkOrder: require("./workOrder")
 }
