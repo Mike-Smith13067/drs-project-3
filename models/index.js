a4e972894e3790094208f9043cbaccfaad75dd16
Mike <hockeydad66@live.com>
2020-03-27 12:57:54 -0600

    fixed typo

--- models/index.js
+++ models/index.js
@@ -2,5 +2,5 @@ module.exports = {
     Customer: require("./customer.js"),
     Inventory: require("./inventory.js"),
     Admin: require("./admin.js"),
-    WorkOrder: require("./workorder.js")
+    WorkOrder: require("./workOrder.js")
 }
